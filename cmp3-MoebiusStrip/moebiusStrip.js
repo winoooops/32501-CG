0092ed58e2513f618fd6e780d9d485d3397a23ed
winoooops <winoooops@outlook.com>
2020-04-02 15:04:07 +1100

    works in the dumbest way

--- cmp3-MoebiusStrip/moebiusStrip.js
+++ cmp3-MoebiusStrip/moebiusStrip.js
@@ -6,25 +6,20 @@ var camera  = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHe
 var renderer = new THREE.WebGLRenderer( )
 renderer.setSize( window.innerWidth, window.innerHeight )
 var scene  = new THREE.Scene() 
+var cubes = [] // craete an array of cubes
+var n = 36
+
+
+
 
 /***************************************************
  * Step 1: settting the playground
  ***************************************************/
 
 var init = function () {
-    var geometry = new THREE.BoxGeometry()
-    var material = new THREE.MeshBasicMaterial({ 
-        color: 0x00ff00, 
-        wireframe: true
-    })
-    var cubeMesh = new THREE.Mesh(geometry, material)
-    cubeMesh.position.set(1, 1, 1)
-    scene.add( cubeMesh )
-    camera.position.z = 5 
+    camera.position.z = 15 
     camera.lookAt(0, 0, 1)
-
     window.addEventListener('resize', resizeWin )
-
     document.body.appendChild( renderer.domElement)
     render() 
 }
@@ -33,6 +28,49 @@ var render = function () {
     renderer.render(scene, camera)
 }
 
+var createScene = function() {
+    for(i = 0; i < n ; i++ ) {
+        //all the trasformation are 4*4 matrix
+        var newCube = createCube(i)
+        scene.add( newCube )
+    }
+}
+
+createScene() 
+
+function createCube( index ) {
+    var rot2 = new THREE.Matrix4() 
+    var sca = new THREE.Matrix4() 
+    var rot = new THREE.Matrix4() 
+    var tra = new THREE.Matrix4() 
+    var combined = new THREE.Matrix4() 
+
+    // scala matrix to disrot the cube to tile elongated 
+    sca.makeScale(0.5,3,1.5)
+    
+
+    rot2.makeRotationZ( index*(Math.PI/n) )
+
+    tra.makeTranslation(10,0,0)
+
+    rot.makeRotationY( index*(2*Math.PI/n ) )
+
+    combined.multiply( rot )
+    combined.multiply( tra )
+    combined.multiply( rot2 )
+    combined.multiply( sca )
+
+    // create cube
+    var geometry = new THREE.BoxGeometry(1,1,1)
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00, 
+        wireframe: true
+    })
+    cubes[index] = new THREE.Mesh(geometry, material)
+    // apply the transformation 
+    cubes[index].applyMatrix( combined )
+    return cubes[index]
+}
 
 /***************************************
  * Step2: listen to resize event on window
